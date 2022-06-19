a91f2346bf31916af0a891b6630f1729f579536c
xiaohejie <2234842317@qq.com>
2022-06-19 21:08:09 +0800

    2022-6-19

--- 01-动态规划DP/01-动态规划DP/91-解码方法.cpp
+++ 01-动态规划DP/01-动态规划DP/91-解码方法.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 /*
 	91-解码方法:
@@ -10,6 +11,23 @@ using namespace std;
 class Solution {
 public:
 	int numDecodings(string s) {
-
+		//设置动态容器
+		int len = s.size();
+		vector<int> f(len + 1);
+		//进行初始化
+		f[0] = 1;		//当s为空时
+		s = ' ' + s;
+		//状态转移方程
+		for (int i = 1; i <= len; i++) {
+			int a = s[i] - '0';				//个位数
+			int b = (s[i - 1] - '0') * 10 + a;	//十几的时候
+			if (a > 0 && a <= 9) {
+				f[i] = f[i - 1];
+			}
+			if (b >= 10 && b <= 26) {
+				f[i] += f[i - 2];
+			}
+		}
+		return f[len];
 	}
 };
